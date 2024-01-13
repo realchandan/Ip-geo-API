efb7a077397d405b549787b91b5419cfcebc1faf
realchandan <contact@chandan.cc>
2024-01-14 00:40:50 +0530

    Refactor binary search algorithm in search.go

--- search.go
+++ search.go
@@ -18,74 +18,11 @@ func Binary(array []IpItem, target *big.Int, lowIndex int, highIndex int) (int, 
 		return -1, ErrNotFound
 	}
 	mid := int(lowIndex + (highIndex-lowIndex)/2)
-	if array[mid].start.Cmp(target) == +1 {
-		if array[mid].end.Cmp(target) == -1 {
-			return mid, nil
-		} else {
-			return Binary(array, target, lowIndex, mid-1)
-		}
-	} else if array[mid].start.Cmp(target) == -1 {
-		if array[mid].end.Cmp(target) == +1 {
-			return mid, nil
-		} else {
-			return Binary(array, target, mid+1, highIndex)
-		}
+	if array[mid].start.Cmp(target) == 1 {
+		return Binary(array, target, lowIndex, mid-1)
+	} else if array[mid].end.Cmp(target) == -1 {
+		return Binary(array, target, mid+1, highIndex)
 	} else {
 		return mid, nil
 	}
-}
-
-func BinaryIterative(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] > target {
-			endIndex = mid - 1
-		} else if array[mid] < target {
-			startIndex = mid + 1
-		} else {
-			return mid, nil
-		}
-	}
-	return -1, ErrNotFound
-}
-
-func LowerBound(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] < target {
-			startIndex = mid + 1
-		} else {
-			endIndex = mid - 1
-		}
-	}
-
-	if startIndex >= len(array) {
-		return -1, ErrNotFound
-	}
-	return startIndex, nil
-}
-
-func UpperBound(array []int, target int) (int, error) {
-	startIndex := 0
-	endIndex := len(array) - 1
-	var mid int
-	for startIndex <= endIndex {
-		mid = int(startIndex + (endIndex-startIndex)/2)
-		if array[mid] > target {
-			endIndex = mid - 1
-		} else {
-			startIndex = mid + 1
-		}
-	}
-
-	if startIndex >= len(array) {
-		return -1, ErrNotFound
-	}
-	return startIndex, nil
 }
